c6efb4adc31a4f5c97e6f9cfc02a48b372384e32
mgrinko <glukym@gmail.com>
2019-04-10 10:00:28 +0300

    Updates

---
+++ precache-manifest.d707101c4bd1ab3e0e13e14e5cdd567e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fac607a281de4f0dee0",
+    "url": "/login-app/static/css/main.e49a6c0e.chunk.css"
+  },
+  {
+    "revision": "9fac607a281de4f0dee0",
+    "url": "/login-app/static/js/main.5b9e5465.chunk.js"
+  },
+  {
+    "revision": "0d595c1f2f62de080ab0",
+    "url": "/login-app/static/js/runtime~main.8a02c311.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/login-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "88f2466740889891847597f71addc8cc",
+    "url": "/login-app/index.html"
+  }
+];
